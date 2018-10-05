975e7b8781afdb8942764b95110c6ad005f1af4d
ganzorigbattur <31978292+ganzorigbattur@users.noreply.github.com>
2018-10-05 18:17:18 -0500

    Update main.c
    final main.c

--- Project1_make4061/main.c
+++ Project1_make4061/main.c
@@ -1,12 +1,9 @@
 /* CSci4061 F2018 Assignment 1
-
 login: cselabs_login_name ()
 date: 10/05/2018
 Ganzorig Battur, Patrice Martineau, Raymond Holidjaja
 id: battu010, mart3676, holid019
-
 This is the main file of the code */
-
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -20,7 +17,7 @@ This is the main file of the code */
 
 /*-------------------------------------------------------HELPER FUNCTIONS PROTOTYPES---------------------------------*/
 void show_error_message(char * ExecName);
-void helper1( target_t targets[], char * targetNamee, int nTargetCount1);
+int helper1( target_t targets[], char * targetNamee, int nTargetCount1, int orignalTarget);
 //Write your functions prototypes here
 void show_targets(target_t targets[], int nTargetCount);
 /*-------------------------------------------------------END OF HELPER FUNCTIONS PROTOTYPES--------------------------*/
@@ -41,12 +38,23 @@ void show_error_message(char * ExecName)
 //Write your functions here
 
 //Phase1: Warmup phase for parsing the structure here. Do it as per the PDF (Writeup)
+
 void show_targets(target_t targets[], int nTargetCount)
 {
-	//Write your warmup code here
+	for (int i = 0; i < nTargetCount; i++) {
+		printf("Target Name: %s \n", targets[i].TargetName);
 
+		printf("Dependency Count: %d\n", targets[i].DependencyCount );
+
+		printf("Dependency Names:" );
+		for(int j = 0; j < targets[i].DependencyCount-1; j++){
+			printf(" %s,", targets[i].DependencyNames[j]);
+		}
+		printf(" %s\n", targets[i].DependencyNames[targets[i].DependencyCount-1]);
+
+		printf("Command: %s \n\n", targets[i].Command);
+	}
 }
-
 /*-------------------------------------------------------END OF HELPER FUNCTIONS-------------------------------------*/
 
 
@@ -73,27 +81,27 @@ int main(int argc, char *argv[])
   //Ex. f: for h there won't be any argument hence we are not going to do the same for h, hence "f:h"
   while((ch = getopt(argc, argv, format)) != -1)
   {
-	  switch(ch)
-	  {
-	  	  case 'f':
-	  		  temp = strdup(optarg);
-	  		  strcpy(Makefile, temp);  // here the strdup returns a string and that is later copied using the strcpy
-	  		  free(temp);	//need to manually free the pointer
-	  		  break;
+	 switch(ch)
+	 {
+	 	 case 'f':
+	 		 temp = strdup(optarg);
+	 		 strcpy(Makefile, temp);  // here the strdup returns a string and that is later copied using the strcpy
+	 		 free(temp);	//need to manually free the pointer
+	 		 break;
 
-	  	  case 'h':
-	  	  default:
-	  		  show_error_message(argv[0]);
-	  		  exit(1);
-	  }
+	 	 case 'h':
+	 	 default:
+	 		 show_error_message(argv[0]);
+	 		 exit(1);
+	 }
 
   }
 
   argc -= optind;
   if(argc > 1)   //Means that we are giving more than 1 target which is not accepted
   {
-	  show_error_message(argv[0]);
-	  return -1;   //This line is not needed
+	 show_error_message(argv[0]);
+	 return -1;   //This line is not needed
   }
 
   /* Init Targets */
@@ -101,13 +109,13 @@ int main(int argc, char *argv[])
 
   /* Parse graph file or die, This is the main function to perform the toplogical sort and hence populate the structure */
   if((nTargetCount = parse(Makefile, targets)) == -1)  //here the parser returns the starting address of the array of the structure. Here we gave the makefile and then it just does the parsing of the makefile and then it has created array of the nodes
-	  return -1;
+	 return -1;
 
 
   //Phase1: Warmup-----------------------------------------------------------------------------------------------------
   //Parse the structure elements and print them as mentioned in the Project Writeup
   /* Comment out the following line before Phase2 */
-  show_targets(targets, nTargetCount);
+  //show_targets(targets, nTargetCount);
   //End of Warmup------------------------------------------------------------------------------------------------------
 
   /*
@@ -130,8 +138,8 @@ int main(int argc, char *argv[])
 
   //Phase2: Begins ----------------------------------------------------------------------------------------------------
   /*Your code begins here*/
-printf("hello\n");
-helper1(targets, TargetName, nTargetCount);
+//calling recursive helper1 function
+helper1(targets, TargetName, nTargetCount, 1);
 
 
   /*End of your code*/
@@ -141,107 +149,92 @@ helper1(targets, TargetName, nTargetCount);
 }
 /*-------------------------------------------------------END OF MAIN PROGRAM------------------------------------------*/
 
-void helper1( target_t targets[], char * targetNamee, int nTargetCount1){
-																	//find the target exist in targets array
+int helper1( target_t targets[], char * targetNamee, int nTargetCount1, int orignalTarget){
+
+
+	//find the target exist in targets array
 	int result = find_target(targetNamee, targets, nTargetCount1);
 	target_t myTarget = targets[result];
-
   if (result == -1) {
-																			//target not exit then error
-		printf("error\n" );
-	} else {
-
-		for (int j = 0; j < myTarget.DependencyCount; j++) {
-																			//distinguish .o and .c file
+		//target not exit then print error
+		printf("error target does not exist\n" );
+		return -1;
+	}else{
+		//for each dependency of target we check do we need to recursive call or not.
+		for(int j = 0; j < myTarget.DependencyCount; j++){
+			//distinguish target file and not target files
 			int isDepTarget = find_target(myTarget.DependencyNames[j], targets, nTargetCount1);
 
-			if(isDepTarget == -1){					// not target case
+			if(isDepTarget == -1){// not target case. Then file should exist
 				int result2 = does_file_exist(myTarget.DependencyNames[j]);
-
-				if (result2 == -1) {				// file doesn't exist error
-					printf("error\n" );
+				if (result2 == -1){
+					//file does not exist error
+					printf("error: %s file not there\n", myTarget.DependencyNames[j] );
+					return -1;
 				}
-			} else { 											// dependency is target
-				pid_t childPIDorZero = fork();
+			}else{ // dependency is target
+					int childresult = helper1(targets, myTarget.DependencyNames[j], nTargetCount1, 0);
+					//checking was the recursive call succesful or was execvp failed and Dependency does not exist
+					if (childresult == -1 || does_file_exist(myTarget.DependencyNames[j]) == -1){
+						if (orignalTarget == 1){
+							printf("failed to execute\n");
+						}
+						return -1;
+					}
+			}
+		}
 
+
+
+		int resultFileExist = does_file_exist(targetNamee);
+		int isNeedToUpDate = 0;
+		if(resultFileExist != -1){ // file compiled before
+						//find edited file and compare with this file time
+
+						for(int j = 0; j < myTarget.DependencyCount; j++){
+							// comparing target modified time with each one of dependencies modified time.
+							int modficationNeed = compare_modification_time(targetNamee, myTarget.DependencyNames[j]);
+							if(modficationNeed == -1){
+								//file does not exist
+								printf("error one of comparing file does not exist\n");
+								return -1;
+							}else if(modficationNeed == 2){
+								//up to date
+								isNeedToUpDate = 1;
+							}
+			 			}
+		}else{
+			// target never been compiled before.
+			isNeedToUpDate =1;
+		}
+		if(isNeedToUpDate == 1){
+			// for to call execvp in different processer
+				pid_t childPIDorZero = fork();
 				if (childPIDorZero < 0) {
 					perror("fork() error");
 					exit(-1);
+					return -1;
 				}
-
 				if (childPIDorZero != 0) {
-					wait(NULL); 							// wait to join w/ parent
+
+					wait(NULL); // wait to join w/ parent
 				} else {
-					helper1(targets, myTarget.DependencyNames[j], nTargetCount1);
+					//child processer for execvp call
+					printf("%s\n", myTarget.Command);
+					char *argvs[ARG_MAX];
+					char * delimm = " ";
+
+					int indexOfArg = parse_into_tokens(myTarget.Command, argvs, delimm);
+
+					int isExecFailed = execvp(argvs[0], argvs);
 					exit(0);
 				}
-
-
-			}
-		}
-
-		int resultFileExist = does_file_exist(targetNamee);
-		int isNeedToUpDate = 1;
-
-		if(resultFileExist != -1){ 		// file compiled before
-		 												//find edited file and compare with this file time
-
-		 	for (int j = 0; j < myTarget.DependencyCount; j++){
- 				int modficationNeed = compare_modification_time(targetNamee, myTarget.DependencyNames[j]);
-
-				if (modficationNeed == -1){ 	// set dependency modification bool
-					//file does not exist
-					isNeedToUpDate = 0;
-					printf("error\n");
-				} else if (modficationNeed == 0 || modficationNeed == 1){
-					isNeedToUpDate = 0;				// update not needed
-					printf("Up to date\n" );
+		}else {
+				// only print "up to date" once at root of recursive calls
+				if (orignalTarget == 1){
+					printf("Up to date\n");
 				}
- 			}
-
-		if(isNeedToUpDate == 1){            // check if dependency is updated
-			printf("exec call:  %s %d\n", myTarget.Command, getpid());
-			char *argvs[ARG_MAX];
-			char * delimm = " ";
-			int indexOfArg = parse_into_tokens(myTarget.Command, argvs, delimm);
-			printf("%s\n",argvs[indexOfArg]); // why null not show up when we print?
-
-			char* commandArray[indexOfArg];
-			char* cmdd = argvs[0];
-			printf("%s\n",cmdd );
-
-			for(int k = 0; k < indexOfArg; k++){
-				printf("       %d %d\n", indexOfArg, k);
-				commandArray[k] = argvs[k+1];
-				printf("%s\n", argvs[k+1]);
-			}
-
-			execvp(cmdd, commandArray);
-			//execvp(argvs[0], argvs);
-
-			}
-
-		} else {
-																// print args similar to make
-			printf("exec call:  %s %d\n", myTarget.Command, getpid());
-			char *argvs[ARG_MAX];
-			char * delimm = " ";
-			int indexOfArg = parse_into_tokens(myTarget.Command, argvs, delimm);
-			printf("%s\n",argvs[indexOfArg]);
-
-			char* commandArray[indexOfArg];
-			char* cmdd = argvs[0];
-			printf("%s\n", cmdd);
-
-			for(int k = 0; k < indexOfArg; k++){
-				commandArray[k] = argvs[k+1];
-				printf("%s\n", argvs[k+1]);
-			}
-
-			printf("%s\n",argvs[indexOfArg]);
-			execvp(cmdd, commandArray);
-			//execvp(argvs[0], argvs);
-			//What is the correct format of execution function? Does it return something when execution failed?
 		}
+		return 1;
 	}
 }
